a0228875f451b80e3d49f990853e9a3220f5075e
bitfishxyz@gmail.com <bitfishxyz@gmail.com>
2019-04-30 23:16:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# SpringMVC Template
+使用项目模板提高开发效率
+
+使用时修改
+- 文件名
+- .gitignore
---
+++ src/main/java/controller/RootController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+    @GetMapping
+    public String root(){
+        return "Hello world";
+    }
+}
